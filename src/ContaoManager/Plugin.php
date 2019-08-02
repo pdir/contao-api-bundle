c5ee364c6144be1574c398e0e9387f821646e66d
Mathias Arzberger <develop@pdir.de>
2019-08-02 17:17:25 +0200

    remove comment

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
- * This file is part of [package name].
- *
- * (c) John Doe
- *
- * @license LGPL-3.0-or-later
- */
-
 namespace Pdir\ApiBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
